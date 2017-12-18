5502cb6b4801966e965e380916cc8a5a974c5b61
Pale Moon <git-repo@palemoon.org>
2017-12-18 01:17:42 +0100

    Add ForEach C++ public function for ES6 Maps/Sets.
    Issue #1546 step 1.

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -4524,6 +4524,9 @@ MapValues(JSContext* cx, HandleObject obj, MutableHandleValue rval);
 extern JS_PUBLIC_API(bool)
 MapEntries(JSContext* cx, HandleObject obj, MutableHandleValue rval);
 
+extern JS_PUBLIC_API(bool)
+MapForEach(JSContext *cx, HandleObject obj, HandleValue callbackFn, HandleValue thisVal);
+
 /*
  * Set
  */
@@ -4553,6 +4556,9 @@ SetValues(JSContext *cx, HandleObject obj, MutableHandleValue rval);
 
 extern JS_PUBLIC_API(bool)
 SetEntries(JSContext *cx, HandleObject obj, MutableHandleValue rval);
+
+extern JS_PUBLIC_API(bool)
+SetForEach(JSContext *cx, HandleObject obj, HandleValue callbackFn, HandleValue thisVal);
 
 } /* namespace JS */
 
